8ec982adfe5209b14fe8eeb2dad6b7c4d648f8b1
Rohit Sehgal <rohitsehgal1994@gmail.com>
2016-09-22 13:05:33 +0530

    Data Structre

--- bst_to_dll.cpp
+++ bst_to_dll.cpp
@@ -14,37 +14,6 @@ void bst_to_dll(treenode<int> *root,dllnode<int> *&head)
       bst_to_dll(root->right,head);
 }
 
-// template<typename type>
-// pair<dllnode<type>*,dllnode<type>*> get_dll_from_bst(treenode<type>* root)
-// {
-//       if(!root)
-//             return pair<dllnode<type>*,dllnode<type>*>(NULL,NULL);
-//
-//       else if(root && !root->left && !root->right)
-//       {
-//             std::cout << "The value of the root " <<root->value <<std::endl;
-//             dllnode<type>*temp = getnodedll(root->value);
-//             return pair<dllnode<type>*,dllnode<type>*>(temp,temp);
-//       }
-//
-//       else
-//       {
-//             dllnode<type>* mainnode = getnodedll(root->value);
-//             cout<<"check1\n";
-//             pair<dllnode<type>*,dllnode<type>*> nodes_from_left = get_dll_from_bst(root->left);
-//             cout<<"check2\n";
-//             (nodes_from_left.second)->next=mainnode;
-//             mainnode->prev=nodes_from_left.second;
-//             cout<<"check3\n";
-//             pair<dllnode<type>*,dllnode<type>*> nodes_from_right = get_dll_from_bst(root->right);
-//             cout<<"check4\n";
-//             (nodes_from_left.first)->prev=mainnode;
-//             cout<<"check5\n";
-//             mainnode->next=nodes_from_left.first;
-//             return pair<dllnode<type>*,dllnode<type>*>(nodes_from_left.first,nodes_from_right.second);
-//       }
-// }
-
 int main()
 {
       treenode<int> *root=NULL;
