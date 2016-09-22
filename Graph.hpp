0bca2f35834f87beddc25bf2ecc67eb5d410fb6d
Rohit Sehgal <rohitsehgal1994@gmail.com>
2016-09-22 15:56:34 +0530

    Graph DS

--- Graph.hpp
+++ Graph.hpp
@@ -1,5 +1,62 @@
-template<typename T>
+#include <iostream>
+using namespace std;
+
+GraphNode* getNode(int n)
+{
+    GraphNode *temp= new GraphNode;
+    temp->node=n;
+    temp->next=NULL;
+}
+
+
+struct GraphNode
+{
+    int node;
+    GraphNode *next;
+};
+
+
 class Graph
 {
-    T *adj_list;
+    GraphNode *adj_list
+    int nodes;
+
+public:
+    Graph(int nodes)
+    {
+        this->nodes = nodes;
+        adj_list =  new GraphNode[nodes];
+        for(int i=0;i<nodes;++i)
+            adj_list[i]=NULL;
+    }
+
+    void add_edge(int u,int v)
+    {
+        GraphNode *temp = getNode(v),*ptr;
+        if(adj_list[u]==NULL)
+            adj_list[u]=temp;
+        else
+        {    while(ptr->next)
+                ptr=ptr->next;
+            ptr->next=temp;
+        }
+    }
+
+    void print()
+    {
+        GraphNode * ptr;
+        for(int i=0;i<nodes;++i)
+        {
+            ptr=adj_list[i];
+            while (ptr) {
+                cout<<ptr->node<<" ";
+            }
+            cout<<endl;
+        }
+    }
+
+    GraphNode * adjacent(int u)
+    {
+        return adj_list[u];
+    }
 }
