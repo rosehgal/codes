d00bbde2d0e7712c5e343c7027e2c5bb7785ee10
Rohit Sehgal <rohitsehgal1994@gmail.com>
2016-09-26 21:35:19 +0530

    Binary Tree

--- diameter_of_tree_optimised.cpp
+++ diameter_of_tree_optimised.cpp
@@ -1,11 +1,29 @@
-#include <iostream>
+#include <bits/stdc++.h>
 #include "binary_tree.hpp"
 
 using namespace std;
 
+pair<int,int> diameter(BinaryTree<int> *root)
+{
+    pair<int,int> results;   //first diameter second height
+
+    if(!root)
+    {
+        results.first=0;
+        results.second=0;
+        return results;
+    }
+
+    pair<int,int> left=diameter(root->left),right=diameter(root->right);
+
+    int dia = max({left.first,right.first,left.second+right.second});
+    int sum = max(left.second,right.second)+1;
+    return pair<int,int>(dia,height);
+}
+
 int main()
 {
     BinaryTree<int> *root = sample_binary_tree();
-    cout<<diameter(root);
+    cout<<diameter(root).first;
     return 0;
 }
