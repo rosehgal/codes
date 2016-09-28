b89caedc5072ca9fb54e5c6d1916f149ee9099cd
Rohit Sehgal <rohitsehgal1994@gmail.com>
2016-09-28 12:27:59 +0530

    String input in reverse

--- lexi_small_with_one_string_reversal_inBinaryString.cpp
+++ lexi_small_with_one_string_reversal_inBinaryString.cpp
@@ -5,7 +5,7 @@ using namespace std;
 //check if string a is smaller than string b
 bool is_small(string a,string b)
 {
-    for(int i=0;i<a.size();++i)
+    for(int i=a.size()-1;i>=0;--i)
         if(a[i]<b[i])
             return true;
     return false;
@@ -14,13 +14,40 @@ bool is_small(string a,string b)
 pair<int,int> reverse_pair(string str)
 {
     int first=-1,last=-1,i;
+    string minstr = str;
+
     for(i=str.size()-1;i>=0;--i)
-        if(str[i]=="b")
+        if(str[i]=='b')
             break;
     first=i;
 
-    for()
+    if(first == -1)
+        return pair<int,int>(-1,-1);
 
+    for(int j=0;j<first;++j)
+    {
+        if(str[j]=='a')
+        {
+            //cout<<j<<endl;
+            string prev = str.substr(0,j);
+            string suff = str.substr(first+1);
+            string mid = str.substr(j,first-j+1);
+            //reverse(mid.begin(),mid.end());
+            string curr="";
+            curr=prev+curr;
+            curr=mid+curr;
+            curr=suff+curr;
+            cout<<curr<<endl;
+            cin.get();
+            cout<<prev<<"\t"<<mid<<"\t"<<suff<<endl;
+            if(is_small(curr,minstr))
+            {
+                minstr=curr;
+                last=j;
+            }
+        }
+    }
+    return pair<int,int>(first,last);
 }
 
 
