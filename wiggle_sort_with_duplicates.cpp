ad21a563bcfc04b2d05eeb83d6287e641dfa2406
Rohit Sehgal <rohitsehgal1994@gmail.com>
2016-09-28 23:08:14 +0530

    Sorting and Array

---
+++ wiggle_sort_with_duplicates.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void wiggleSort(vector<int>& nums)
+ {
+     vector<int> sorted = nums;
+
+     sort(sorted.begin(),sorted.end());
+
+     int upper_limit = ceil((float)nums.size()/2);//,loc=nums.size()-1;
+     cout<<upper_limit<<endl;
+
+     int i=0,j=upper_limit,loc=nums.size()-1;
+     while(i<j && loc>=0)
+     {
+         if(loc%2==1)
+            nums[loc]=sorted[j++];
+        else
+            nums[loc]=sorted[i++];
+        loc--;
+     }
+ }
+
+
+int main()
+{
+
+    vector<int> arr{4,5,5,6};
+    wiggleSort(arr);
+    for(auto &a:arr)
+        cout<<a<<" ";
+    return 0;
+}
